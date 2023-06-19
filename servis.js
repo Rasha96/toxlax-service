6d9db6f72daa23673c4729ab3dca2f02e606102e
Rasha96 <59225318+Rasha96@users.noreply.github.com>
2023-06-19 23:09:13 +0300

    Update servis.js

--- servis.js
+++ servis.js
@@ -1,67 +1,103 @@
-class Trans{
+class Translate{
     constructor(){
         
         document.getElementById("english").addEventListener("click",()=>{
-            this.trans("english");
+            this.translate("english");
         });
         document.getElementById("turkey").addEventListener("click",()=>{
-            this.trans("turkey");
+            this.translate("turkey");
         });
         document.getElementById("arabic").addEventListener("click",()=>{
-            this.trans("arabic");
+            this.translate("arabic");
         });
-        this.trans(localStorage.getItem("language"));
+        this.translate(localStorage.getItem("language"));
     }
-    trans(language){
+    translate(language){
         if(language=="english"){
             document.getElementById("0").innerHTML="home";
             document.getElementById("1").innerHTML="About Us";
             document.getElementById("2").innerHTML="Service";
             document.getElementById("3").innerHTML="contact";
-            document.getElementById("4").innerHTML="Why Us?";
-            document.getElementById("5").innerHTML="What do we do for you?";
-            document.getElementById("6").innerHTML="We are Toxlax Company We provide you with all the necessary requirements in order to maintain contact with customers and develop your business in a way that suits the continuously evolving practical environmen";
-            document.getElementById("7").innerHTML="Hurry up";
-            document.getElementById("8").innerHTML="to get a job";
-            document.getElementById("9").innerHTML="We help you get a job opportunity in several fields in which you can prove your successes";
-            document.getElementById("10").innerHTML="High Quality Work";
-            document.getElementById("11").innerHTML="All our work is executed with the highest degree of efficiency.";
-            document.getElementById("12").innerHTML="Professional Support";
-            document.getElementById("13").innerHTML="Our team works diligently to ensure that all your questions are answered and your needs met.";
-            document.getElementById("14").innerHTML="Qualified Team";
-            document.getElementById("15").innerHTML="Our team is equipped with a diverse set of skills.";
-            document.getElementById("16").innerHTML="What Our Clinets Say?";
-            document.getElementById("17").innerHTML="I honestly could not be more pleased with this product. So creative and helpful.";
-            document.getElementById("18").innerHTML="Toxlax has been wonderful. They have the best customer service and always go above and beyond to answer my questions and make sure my needs are met.";
-            document.getElementById("19").innerHTML="I’ve never been more impressed with any other product before. I’ve tried so many of the other ones, and this is truly the one that stands out the most.";
-          
-         
+            document.getElementById("4").innerHTML="TOXLAX costumer servise";
+            document.getElementById("5").innerHTML="We are here for you";
+            document.getElementById("6").innerHTML="commercial services";
+            document.getElementById("6a").innerHTML="Logo design";
+            document.getElementById("7").innerHTML=" Programming development.";
+            document.getElementById("8").innerHTML=" Advertising industry";
+            document.getElementById("9").innerHTML=" Business advice.";
+            document.getElementById("10").innerHTML="Consulting all business.";
+            document.getElementById("11").innerHTML=" Website design for businesses, online stores and other websites.";
+            document.getElementById("12").innerHTML="Invest your money with the latest modern projects.. ";
+            document.getElementById("13").innerHTML="The best global attempts to sell electricity are on grid devices.";
+            document.getElementById("14").innerHTML="General legal services";
+            document.getElementById("14a").innerHTML="Tourist Residencies";
+            document.getElementById("15").innerHTML="Student Residencies";
+            document.getElementById("16").innerHTML="Work Residencies ";
+            document.getElementById("17").innerHTML="Health Insurance";
+            document.getElementById("18").innerHTML="Flight Reservations";
+            document.getElementById("19").innerHTML="Hotel Reservations";
+            document.getElementById("20").innerHTML="Daily/Monthly Furnished Apartments.";
+            document.getElementById("21").innerHTML="Curative Tourism in collaboration with accredited Turkish healthcare facilities";
+            document.getElementById("22").innerHTML="Students’ Services";
+            document.getElementById("23").innerHTML="Students registration in public universities in all disciplines by signing legal agreement.";
+            document.getElementById("24").innerHTML="rivate universities students’ registration.";
+            document.getElementById("25").innerHTML="Schools students’ registration.";
+            document.getElementById("26").innerHTML="Providing registration services in the Turkish language institute (TOMER).";
+            document.getElementById("27").innerHTML="Airport pick-up services.";
+            document.getElementById("28").innerHTML="Providing dormitory or furnished apartment renting services for the students, that matching the students requirements and needs.";
+            document.getElementById("29").innerHTML=" Providing the needed legal residency and health insurance.";
+            document.getElementById("30").innerHTML="Optaining special universities discounts.";
+            document.getElementById("31").innerHTML="In Addition To VIP Services";
+            document.getElementById("32").innerHTML=" Tourist Residencies";
+            document.getElementById("33").innerHTML="Turkish citizenship";
+            document.getElementById("34").innerHTML="Employment contracts in Europe Toxlax works to provide employment contracts in Poland, where employment contracts are for the purpose of issuing and facilitating visa procedures. Toxlax guarantees 90% of obtaining the visa, provided that the legal requirements for the visa are met";
+            document.getElementById("35").innerHTML="Visa services Europe (Schengen) visa for all nationalities and Canada and USA visa";
+           
        
        
         }
         else if(language=="turkey"){
+           
             document.getElementById("0").innerHTML="Anasayfa";
             document.getElementById("1").innerHTML='Hakımızda';
             document.getElementById("2").innerHTML="hizmetlerimiz";
             document.getElementById("3").innerHTML="iletişim";
-            document.getElementById("4").innerHTML="neden biz";
-            document.getElementById("5").innerHTML="Sizin için ne yapıyoruz?";
-            document.getElementById("6").innerHTML="Biz Toxlax Şirketiyiz Müşterilerle iletişiminizi sürdürmek ve işinizi sürekli gelişen pratik ortama uygun bir şekilde geliştirmek için gerekli tüm gereksinimleri sağlıyoruz.";
-            document.getElementById("7").innerHTML="Acele et!";
-            document.getElementById("8").innerHTML="iş almak için";
-            document.getElementById("9").innerHTML="Başarılarınızı kanıtlayabileceğiniz birçok alanda iş fırsatı bulmanıza yardımcı oluyoruz";
-            document.getElementById("10").innerHTML="Yüksek Kaliteli Çalışma";
-            document.getElementById("11").innerHTML="Tüm çalışmalarımız en yüksek verimlilik derecesi ile yürütülür";
-            document.getElementById("12").innerHTML="Profesyonel Destek";
-            document.getElementById("13").innerHTML="Ekibimiz, tüm sorularınızın yanıtlanmasını ve ihtiyaçlarınızın karşılanmasını sağlamak için özenle çalışır.";
-            document.getElementById("14").innerHTML="Nitelikli Ekip";
-            document.getElementById("15").innerHTML="Ekibimiz çeşitli becerilerle donatılmıştır.";
-            document.getElementById("16").innerHTML="Müşterilerimiz Ne Diyor?";
-            document.getElementById("17").innerHTML="Açıkçası bu şirketten daha fazla memnun kalamazdım. Çok yaratıcı ve yardımsever.";
-            document.getElementById("18").innerHTML="“Toxlax harikaydı. En iyi müşteri hizmetine sahipler ve sorularımı yanıtlamak ve ihtiyaçlarımın karşılandığından emin olmak için her zaman yukarıda ve öteye gidiyorlar.";
-            document.getElementById("19").innerHTML="Daha önce başka hiçbir şirketten bu kadar etkilenmemiştim. Diğerlerinin çoğunu denedim ve bu gerçekten en çok öne çıkanı.";
-            
-       
+            document.getElementById("4").innerHTML="TOXLAX müşteri hizmetleri";
+            document.getElementById("5").innerHTML="Biz senin için buradayız";
+            document.getElementById("6").innerHTML="Ticari hizmetler";
+            document.getElementById("6a").innerHTML="Logo tasarımı";
+            document.getElementById("7").innerHTML="Programlama geliştirme.";
+            document.getElementById("8").innerHTML=" Reklam endüstrisi";
+            document.getElementById("9").innerHTML="İş tavsiyesi.";
+            document.getElementById("10").innerHTML="Tüm iş danışmanlığı.";
+            document.getElementById("11").innerHTML="İşletmeler, çevrimiçi mağazalar ve diğer web siteleri için web sitesi tasarımı.";
+            document.getElementById("12").innerHTML="Paranızı en son modern projelerle yatırın.. ";
+            document.getElementById("13").innerHTML="Elektrik satmaya yönelik en iyi küresel girişimler şebeke cihazlarındadır.";
+            document.getElementById("14").innerHTML="Genel hukuk hizmetleri";
+            document.getElementById("14a").innerHTML="Turistik Rezidanslar";
+            document.getElementById("15").innerHTML="Öğrenci Yurtları";
+            document.getElementById("16").innerHTML="İş Rezidansları ";
+            document.getElementById("17").innerHTML="Sağlık Sigortası";
+            document.getElementById("18").innerHTML="Uçuş Rezervasyonları";
+            document.getElementById("19").innerHTML="Otel rezervasyonları";
+            document.getElementById("20").innerHTML="Günlük/Aylık Eşyalı Daireler.";
+            document.getElementById("21").innerHTML="Akredite Türk sağlık tesisleri ile işbirliği içinde Tedavi Turizmi";
+            document.getElementById("22").innerHTML="Öğrenci Hizmetleri";
+            document.getElementById("23").innerHTML="Devlet üniversitelerinde tüm disiplinlerde yasal sözleşme imzalayarak öğrenci kaydı.";
+            document.getElementById("24").innerHTML="özel üniversite öğrenci kayıtları";
+            document.getElementById("25").innerHTML="Okul öğrencilerinin kayıtları.";
+            document.getElementById("26").innerHTML="Türk Dil Kurumu (TÖMER) nezdinde kayıt hizmeti verilmesi.";
+            document.getElementById("27").innerHTML="Havaalanı karşılama hizmetleri.";
+            document.getElementById("28").innerHTML="Öğrencilerin istek ve ihtiyaçlarına uygun yurt veya eşyalı daire kiralama hizmetinin verilmesi.";
+            document.getElementById("29").innerHTML=" Gerekli yasal ikamet ve sağlık sigortasının sağlanması.";
+            document.getElementById("30").innerHTML="Üniversitelere özel indirimlerden yararlanma.";
+            document.getElementById("31").innerHTML="VIP Hizmetlerine Ek Olarak";
+            document.getElementById("32").innerHTML=" Turistik Rezidanslar";
+            document.getElementById("33").innerHTML="Türk vatandaşlığı";
+            document.getElementById("34").innerHTML="Avrupa'da istihdam sözleşmeleri Toxlax, iş sözleşmelerinin vize prosedürlerini düzenleme ve kolaylaştırma amaçlı olduğu Polonya'da istihdam sözleşmeleri sağlamak için çalışır. Toxlax, vize için yasal gerekliliklerin karşılanması koşuluyla vizenin alınmasının %90'ını garanti eder.";
+            document.getElementById("35").innerHTML="Vize hizmetleri Tüm uluslar için Avrupa (Schengen) vizesi ve Kanada ve ABD vizesi";
+         
+          
            
             
         }
@@ -70,27 +106,44 @@ class Trans{
             document.getElementById("1").innerHTML="من نحن";
             document.getElementById("2").innerHTML="خدمات";
             document.getElementById("3").innerHTML="للتواصل";
-            document.getElementById("4").innerHTML="لماذا توكس لاكس";
-            document.getElementById("5").innerHTML="ماذا نفعل من أجلك؟";
-            document.getElementById("6").innerHTML="نحن شركة توكسلاكس نحن نوفر لك جميع المتطلبات اللازمة من أجل الحفاظ على الاتصال مع العملاء وتطوير أعمالك بطريقة تناسب البيئة العملية المتطورة باستمرار";
-            document.getElementById("7").innerHTML="أسرع";
-            document.getElementById("8").innerHTML="للحصول على وظيفة";
-            document.getElementById("9").innerHTML="نساعدك في الحصول على فرصة عمل في عدة مجالات يمكنك أن تثبت فيها نجاحاتك";
-            document.getElementById("10").innerHTML="عمل عالي الجودة";
-            document.getElementById("11").innerHTML="يتم تنفيذ جميع أعمالنا بأعلى درجات الكفاءة.";
-            document.getElementById("12").innerHTML="دعم احترافي";
-            document.getElementById("13").innerHTML="يعمل فريقنا بجد لضمان الإجابة على جميع أسئلتك وتلبية احتياجاتك.";
-            document.getElementById("14").innerHTML="فريق مؤهل";
-            document.getElementById("15").innerHTML="فريقنا مجهز بمجموعة متنوعة من المهارات.";
-            document.getElementById("16").innerHTML="ماذا يقول عملاؤنا؟";
-            document.getElementById("17").innerHTML="بصراحة لا يمكن أن أكون أكثر سعادة بهذا المنتج. مبدع جدا ومفيد.";
-            document.getElementById("18").innerHTML="لقد كان توكسلاكس رائعًا. لديهم أفضل خدمة عملاء ودائما يذهبون إلى أبعد من ذلك للإجابة على أسئلتي والتأكد من تلبية احتياجاتي.";
-            document.getElementById("19").innerHTML="لم يسبق لي أن أعجبت بأي منتج آخر من قبل. لقد جربت الكثير من الأشياء الأخرى ، وهذا حقًا هو الأكثر تميزًا.";
-            
+            document.getElementById("4").innerHTML=" توكسلاس خدمة عملاء ";
+            document.getElementById("5").innerHTML="نحن هنا من أجلك";
+            document.getElementById("6").innerHTML="خدمات تجارية";
+            document.getElementById("6a").innerHTML="تصميم شعار";
+            document.getElementById("7").innerHTML=" تطوير البرمجة.";
+            document.getElementById("8").innerHTML="مجال الدعاية";
+            document.getElementById("9").innerHTML="نصائح تجارية.";
+            document.getElementById("10").innerHTML="استشارة جميع الأعمال.";
+            document.getElementById("11").innerHTML="تصميم مواقع الويب للشركات والمتاجر عبر الإنترنت ومواقع الويب الأخرى.";
+            document.getElementById("12").innerHTML="استثمر أموالك بأحدث المشاريع الحديثة .. ";
+            document.getElementById("13").innerHTML="أفضل المحاولات العالمية لبيع الكهرباء هي على أجهزة الشبكة.";
+            document.getElementById("14").innerHTML="خدمات قانونية عامة";
+            document.getElementById("14a").innerHTML="الاقامات السياحية";
+            document.getElementById("15").innerHTML="مساكن الطلاب";
+            document.getElementById("16").innerHTML="إقامات العمل ";
+            document.getElementById("17").innerHTML="تأمين صحي";
+            document.getElementById("18").innerHTML="حجوزات الطيران";
+            document.getElementById("19").innerHTML="حجوزات فندقية";
+            document.getElementById("20").innerHTML="شقق مفروشة يومية / شهرية.";
+            document.getElementById("21").innerHTML="السياحة العلاجية بالتعاون مع مرافق الرعاية الصحية التركية المعتمدة";
+            document.getElementById("22").innerHTML="خدمات الطلاب";
+            document.getElementById("23").innerHTML="تسجيل الطلاب في الجامعات الحكومية في جميع التخصصات بتوقيع اتفاقية قانونية.";
+            document.getElementById("24").innerHTML="تسجيل طلاب الجامعات الخاصة.";
+            document.getElementById("25").innerHTML="تسجيل طلاب المدارس.";
+            document.getElementById("26").innerHTML="تقديم خدمات التسجيل في معهد اللغة التركية (تومر).";
+            document.getElementById("27").innerHTML="خدمات الاستقبال في المطار.";
+            document.getElementById("28").innerHTML="توفير خدمات تأجير سكن الطلبة أو الشقق المفروشة بما يتناسب مع متطلبات الطلاب واحتياجاتهم.";
+            document.getElementById("29").innerHTML="توفير الإقامة القانونية اللازمة والتأمين الصحي.";
+            document.getElementById("30").innerHTML="الحصول على خصومات جامعية خاصة.";
+            document.getElementById("31").innerHTML="بالإضافة إلى خدمات كبار الشخصيات";
+            document.getElementById("32").innerHTML=" الاقامات السياحية";
+            document.getElementById("33").innerHTML="الجنسية التركية";
+            document.getElementById("34").innerHTML="عقود العمل في أوروبا تعمل Toxlax على توفير عقود العمل في بولندا ، حيث تكون عقود العمل لغرض إصدار وتسهيل إجراءات التأشيرة. تضمن Toxlax 90٪ من الحصول على التأشيرة ، بشرط استيفاء المتطلبات القانونية للتأشيرة";
+            document.getElementById("35").innerHTML="تأشيرة خدمات التأشيرات الأوروبية (شنغن) لجميع الجنسيات وتأشيرة كندا والولايات المتحدة الأمريكية";
            
         }
         localStorage.setItem("language",language);
 
     }
 }
-onload=new Trans();
+onload=new Translate();
